13847e3b9fccb397e3d26aa84a7e615d91d8c505
Matt Jadud <matthew.jadud@gsa.gov>
2023-07-08 16:32:53 -0400

    Broke out the template
    Now, it is possible to pass in a template file, and mark
    
    <!-- TITLE -->
    
    for where the title should be inserted, and
    
    <!-- MAIN -->
    
    where the body content should be inserted.

--- main.rkt
+++ main.rkt
@@ -10,7 +10,11 @@
 
 
 (define (process-openapi #:url url
-                         #:destination destination)
+                         #:destination-filename destination
+                         #:template-filename template-filename
+                         #:title [title "FAC API version ~a documentation"]
+                         #:version [version "1.0.0"]
+                         )
   (define res (get url))
   (cond
     [(= 200 (response-status-code res))
@@ -18,23 +22,40 @@
     [(= 400 (response-status-code res))
      (printf "Bad response: 400~n")
      (exit 1)])
+
   (define body (response-body res))
   (response-close! res)
+
   (define json
     (with-input-from-string (bytes->string/utf-8 body)
       (λ () (read-json))))
-  (define assembled-document
-    (template #:endpoints (map sorted-key
-                               (stream->list (pairs-in-order (hash-ref json 'definitions))))
-              #:definitions (hash-ref json 'definitions)                              
-              ))
 
-  (define outp (open-output-file destination #:mode 'text #:exists 'replace))
-  (display "<!DOCTYPE html>" outp)
-  (display-xml/content (xexpr->xml assembled-document)
-                       outp
+  (define assembled-document
+    (render #:endpoints (map sorted-key
+                               (stream->list (pairs-in-order (hash-ref json 'definitions))))
+              #:definitions (hash-ref json 'definitions)
+              #:title title
+              #:version version
+              ))
+  (define templatep (open-input-file template-filename))
+
+  (define outfp (open-output-file destination #:mode 'text #:exists 'replace))
+
+  (display "<!DOCTYPE html>" outfp)
+  (for ([line (port->lines templatep)])
+    (cond
+      [(regexp-match "<!-- TITLE -->" line)
+       (displayln (format title version) outfp)
+       ]
+      [(regexp-match "<!-- MAIN -->" line)
+       (display-xml/content (xexpr->xml assembled-document)
+                       outfp
                        #:indentation 'classic)
-                       
+       ]
+      [else
+       (displayln line outfp)
+       ]))
+  (close-output-port outfp)
   )
 
 (provide process-openapi)
@@ -45,6 +66,7 @@
   
   (define default-url "https://fac-dev-postgrest.app.cloud.gov/")
   (define default-destination "fac-api-documentation.html")
+  (define default-template "template.html")
 
   
   (flag (openapi-url url)
@@ -54,11 +76,16 @@
   (flag (destination-file fname)
         ("-o" "--outfile" "Output filename (.html)")
         (destination-file fname))
+
+  (flag (template-file tfname)
+        ("-t" "--template" "Template filename")
+        (template-file tfname))
   
   (program (openapi-to-uswds)
            (process-openapi
             #:url (or (openapi-url) default-url)
-            #:destination (or (destination-file) default-destination)
+            #:destination-filename (or (destination-file) default-destination)
+            #:template-filename (or (template-file) default-template)
             ))
   
   (run openapi-to-uswds))
--- template.rkt
+++ template.rkt
@@ -1,42 +1,26 @@
 #lang racket
 (require "base.rkt")
-(provide template)
+(provide render)
 
 (provide (all-defined-out))
 
-(define (template #:title [title "FAC API version ~a documentation"]
-                  #:version [version "1.0.0"]
-                  #:endpoints [endpoints '()]
+(define (render #:endpoints [endpoints '()]
                   #:definitions [definitions (hash)]
+                  #:title title
+                  #:version version
                   )
-  `(html
-    (head
-     (meta ([charset "UTF-8"]))
-     (meta ([http-equiv "X-UA-Compatible"]
-            [content "IE=edge"]))
-              
-     (script ([src "assets/uswds/dist/js/uswds-init.min.js"]))
-     (link ([rel "stylesheet"]
-            [href "assets/uswds/dist/css/uswds.css"]
-            ))
-     (meta ([name "viewport"]
-            [content "width=device-width, initial-scale=1.0"]))
-     (title ,(~a (format title version)))
-     )
-    (body
-     (script ([src "assets/uswds/dist/js/uswds.min.js"]))
-     (div ([class "usa-section"])
-          (div ([class "grid-container"])
-               (div ([class "grid-row grid-gap"])
-                    (main ([class "usa-layout-docs__main desktop:grid-col-9 usa-prose usa-layout-docs"]
-                           [id "main-content"])
-                          (h1 ,(format title version))
-                          (h2 ([id "api-endpoints"])
-                              "API endpoints")
-                          ,(endpoints->index endpoints)
-                          ,(render-endpoints endpoints definitions)
-                          ))))
-     )))
+  ` (div ([class "usa-section"])
+         (div ([class "grid-container"])
+              (div ([class "grid-row grid-gap"])
+                   (main ([class "usa-layout-docs__main desktop:grid-col-9 usa-prose usa-layout-docs"]
+                          [id "main-content"])
+                         (h1 ,(format title version))
+                         (h2 ([id "api-endpoints"])
+                             "API endpoints")
+                         ,(endpoints->index endpoints)
+                         ,(render-endpoints endpoints definitions)
+                         ))))
+  )
 
 ;;;;;
 
@@ -57,30 +41,37 @@
   (define pieces
     (for/list ([endpoint endpoints]
                [ndx (length endpoints)])
-        (define toggle-target (format "accordion-endpoint-~a" ndx))
-        `(div
-          (a ([name ,(format "endpoint-~a" endpoint)]))
-          (h4 ([class "usa-accordion__heading"])
-              (button ([type "button"]
-                       [class "usa-accordion__button"]
-                       [aria-expanded "false"]
-                       [aria-controls ,toggle-target])
-                      ,(~a endpoint)))
-          (div ([id ,toggle-target]
-                [class "usa-accordion__content usa-prose"])
-               (p ,(~a (deep-ref definitions 'description)))
-               (h3 "Fields")
-               ,@(for/list ([ffp (pairs-in-order (deep-ref definitions endpoint 'properties))])
-                   `(div ([class "grid-row"])
-                         (div ([class "grid-col-4"])
-                              (h4 ,(~a (sorted-key ffp))))
-                         (div ([class "grid-col-2"])
-                              (br)
-                              (span ([class "usa-tag"])
-                                    ,(~a (hash-ref (sorted-value ffp) 'type ""))))
-                         (div ([class "grid-col-6"])
-                              (p ,(~a (hash-ref (sorted-value ffp) 'description "")))))
-                   )))))
+      (define toggle-target (format "accordion-endpoint-~a" ndx))
+      `(div
+        (a ([name ,(format "endpoint-~a" endpoint)]))
+        ;; Endpoint name
+        (h3 ,(~a endpoint))
+        ;; Collapse the parameters
+        (h4 ([class "usa-accordion__heading"]
+             )
+            (button ([type "button"]
+                     [class "usa-accordion__button"]
+                     [aria-expanded "false"]
+                     [aria-controls ,toggle-target]
+                     )
+                    ,(~a (format "Query params on the ~a endpoint"
+                                 endpoint
+                                 ))))
+        (div ([id ,toggle-target]
+              [class "usa-accordion__content usa-prose"])
+             (p ,(~a (deep-ref definitions 'description)))
+             (h3 "Fields")
+             ,@(for/list ([ffp (pairs-in-order (deep-ref definitions endpoint 'properties))])
+                 `(div ([class "grid-row"])
+                       (div ([class "grid-col-4"])
+                            (h4 ,(~a (sorted-key ffp))))
+                       (div ([class "grid-col-2"])
+                            (br)
+                            (span ([class "usa-tag"])
+                                  ,(~a (hash-ref (sorted-value ffp) 'type ""))))
+                       (div ([class "grid-col-6"])
+                            (p ,(~a (hash-ref (sorted-value ffp) 'description "")))))
+                 )))))
 
   `(div ([class "usa-accordion"])
         ,@pieces
